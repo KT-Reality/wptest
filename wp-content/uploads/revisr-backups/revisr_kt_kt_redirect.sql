fef10df86ec782b3c63c4b62599c5004620a2919
adminkt <komalchand.gaidhane@realitypremedia.com>
2017-06-26 18:01:06 +0530

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_kt_kt_redirect.sql
+++ wp-content/uploads/revisr-backups/revisr_kt_kt_redirect.sql
@@ -31,12 +31,12 @@ CREATE TABLE `kt_kt_redirect` (
   `cta_position` varchar(50) DEFAULT NULL,
   `cta_bt_status` tinyint(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `kt_kt_redirect` WRITE;
 /*!40000 ALTER TABLE `kt_kt_redirect` DISABLE KEYS */;
-INSERT INTO `kt_kt_redirect` VALUES (6,'0000-00-00 00:00:00','Excude Redirect','http://192.168.100.53/wptest/sample-page','','','','','','','50,61',0,'','','','7',0);
+INSERT INTO `kt_kt_redirect` VALUES (20,'0000-00-00 00:00:00','Excude Redirect','http://192.168.100.53/wptest/sample','','','','','','','1,26,61',0,'','','','4,7,8',1);
 /*!40000 ALTER TABLE `kt_kt_redirect` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
