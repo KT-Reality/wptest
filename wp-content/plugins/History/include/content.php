13da08943537ae20825cc32a8452242ad7d55447
adminkt <komalchand.gaidhane@realitypremedia.com>
2017-06-27 18:16:59 +0530

    Daily backup - June 27, 2017

--- wp-content/plugins/History/include/content.php
+++ wp-content/plugins/History/include/content.php
@@ -8,7 +8,8 @@ $args = array( 'post_type' => 'history_post', 'posts_per_page' => -1, 'meta_key'
 					$date = date_create($history_date);
 				    $date_formate = date_format($date,"Y/m/d");
 				    $history_date_value = date("d/m/Y", strtotime($date_formate));
-					$content .='<li data-date="'.esc_attr($history_date_value).'"><h2>'.esc_attr(get_the_title()).'</h2>'.esc_attr(get_the_content()).'</li>';
+					//apply_filters( 'the_content', get_the_content() );
+					$content .='<li data-date="'.esc_attr($history_date_value).'"><h2>'.esc_attr(get_the_title()).'</h2>'.get_the_content().'</li>';
 					/*echo $i;*/
 					/*$i = date('d/m/Y',strtotime("+1 week", strtotime($i)));*/
 					/*$i++;*/
